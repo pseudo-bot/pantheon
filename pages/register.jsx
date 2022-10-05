6f17ff05d966a2c7019c2acbe891f226d9fb63a6
lohit244 <lohit244@gmail.com>
2022-10-05 11:55:02 +0530

    Left Nav Component for Registerarion Page

---
+++ pages/register.jsx
@@ -0,0 +1,9 @@
+import LeftNavContainer from "../components/Registeration/LeftNavContainer";
+
+export default function Register() {
+  return (
+    <LeftNavContainer currentPage={0}>
+      <div> Test </div>
+    </LeftNavContainer>
+  );
+}
