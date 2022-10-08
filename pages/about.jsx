3c295ad8727eb6ef8f78dd96fc2f2b7e14bc099c
lohit244 <lohit244@gmail.com>
2022-10-08 22:25:20 +0530

    About page added

---
+++ pages/about.jsx
@@ -0,0 +1,77 @@
+export default function About() {
+	return (
+		<div className="mt-16">
+			<div className="sm:text-5xl text-4xl pt-8 text-gray-100 uppercase text-center font-bold">
+				About
+			</div>
+
+			<div className="text-white text-justify sm:w-8/12 mx-auto mt-8">
+				With a zeal to strive for excellence and to spark the interest of
+				student towards technical prowess and ingenious innovations, BIT Mesra
+				conducts one of its biggest technical fests of Eastern India-
+				"PANTHEON". The fest aims for a humble beginning-towards potentiating
+				our society- through technology- to engender a better tomorrow. It
+				provides an arena for technological competition, deep cerebrating and
+				innovation. The Fest provides a platform to showcase your aptitude and
+				compete with others in terms of cognizance. This edition of Pantheon
+				delved deeper into the astounding world of technology through its
+				enthralling theme of "Evolution to the eternity”. From October 15 to 17
+				2022,tech aesthete and aficionado will amass together to show the magic
+				with their hats of thoughts. Many exquisite events and seminar will be
+				organised in the fields of scientific research, coding, robotics, public
+				speaking and quizzes. The flagship events include the Sci-Tech quiz: An
+				insightful and riveting quiz on topics related to science and
+				technology, Roboquiz: capability to imitate intelligent human behaviour,
+				it's all about AI! and to incorporate that in human life is a speciality
+				of robotics. Insight-out : - A fun event of 2 rounds where participants
+				will act and write in accordance with the character assigned. Dev Hacks
+				: to show off your dev skills to the world , beat the other teams and
+				complete the tasks before the time is over. Apart from that, Pantheon
+				'22 will offer a manifold of entertainment activities such as gaming
+				events, debates, presentations etc, providing fun entertainment along
+				with a phrenic podium for the victors.
+			</div>
+
+			<div className="sm:text-5xl text-4xl pt-8 mt-12 text-gray-100 uppercase text-center font-bold">
+				Theme: Evolution to Eternity
+			</div>
+
+			<div className="text-white text-justify sm:w-8/12 mx-auto my-8">
+				Not the most intelligent species survive. It is not the fittest that
+				survives, but the species that survive are those that adjust and
+				acclimate best to a transmuting and challenging environment, Charles
+				Darwin said in "The Origin of Species”. All living things are cognate
+				through their descendants from a common ancestor. Evolution is “the
+				process by which structural rearrangements are affected over time,
+				ultimately producing forms or structures that are qualitatively
+				different from ancestral forms.” Evolution is not just a biological
+				phenomenon; it is a perpetual vicissitude in human life from the
+				molecular level to the socio-cultural level. Continued evolution is
+				necessary, and more concretely "Evolution: to the Eternity" is the only
+				way humanity will survive this expeditious ecumenical change.
+				Pantheon'22 aims to celebrate the paramount of evolution in society,
+				systems and a common life. The evolution of our psyche is illuminated by
+				our individual vigils into the dark background of eternity.
+			</div>
+
+			{/* <div className="sm:text-5xl text-4xl pt-8 mt-12 text-gray-100 uppercase text-center font-bold">
+				Social Cause
+			</div>
+
+			<div className="text-white text-justify sm:w-8/12 mx-auto my-8">
+				The future is green energy; sustainable and renewable. So PANTHEON is
+				back with its innovative social cause of "Sustainergy" in order to bring
+				out the best expertise and technology guidance to abbreviate or
+				eliminate waste, amend and forefend air quality and the environment,
+				engender sustainable energy, and increment revenue. Clean energy is an
+				astute investment. Renewables make urban energy infrastructures more
+				independent from remote sources and grids. To transmute perspective of
+				students and to give their fullest, this social cause will allow them to
+				cerebrate of processes to engender long-term social and environmental
+				benefits while engendering economic profits for the firm. This will
+				avail them to engender clean energy in a perspicacious way without
+				harnessing any fossil fuels.
+			</div> */}
+		</div>
+	);
+}
