6a85256d8f5af6ceea1e55832e0d1d71506a91f0
Ravish Raj Tiwary <ravishrajtiwary21@gmail.com>
2022-10-09 13:56:45 +0530

    spnoser page

---
+++ pages/sponsors.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const sponsors = () => {
+  return (
+    <div className="mt-16 pb-8 min-h-[calc(100vh-4rem)] h-max text-white bg-neutral-900">
+
+      <div className="sm:text-5xl text-5xl pt-8 text-gray-100 uppercase text-center font-bold">
+        Hiring Sponsors
+      </div>
+
+
+      <div className="pt-8 flex flex-row flex-wrap md:w-3/4 mx-auto justify-evenly gap-y-8">
+
+      <img className="rounded-t-lg max-w-sm mt-4" src= "/sponsors/zuno.png" alt="" />
+
+      </div>
+
+    </div>
+  )
+}
+
+export default sponsors
