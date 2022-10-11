a19fbf4507fef278ac207a0fd2bbaae27261108b
lohit244 <lohit244@gmail.com>
2022-10-11 14:17:38 +0530

    progress bar, animation on events page

--- components/Events/card.jsx
+++ components/Events/card.jsx
@@ -1,50 +1,53 @@
 import { useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export default function Card({
-	imgsrc,
-	name,
-	club,
-	datetime,
-	venue,
-	coordinators,
-	tsize,
-	desc,
+  imgsrc,
+  name,
+  club,
+  datetime,
+  venue,
+  coordinators,
+  tsize,
+  desc,
 }) {
-	const [showDesc, setShowDesc] = useState(false);
-	return (
-		<div className="flex justify-center">
-			<div className="rounded-lg shadow-lg bg-neutral-800 max-w-sm">
-				<img className="rounded-t-lg" src={imgsrc} alt="" />
-				<div className="p-6">
-					<h5 className="text-white text-xl font-bold mb-2">{name}</h5>
-					<h5 className="text-gray-400 text-base font-bold mb-2">{datetime}</h5>
-					<p className="text-gray-400 text-base mb-4">
-						{showDesc ? (
-							desc
-						) : (
-							<>
-								Club - {club}
-								<br />
-								Team Size - {tsize}
-								<br />
-								Venue - {venue}
-								<br />
-								Coordinators - {coordinators}
-							</>
-						)}
-					</p>
-					{desc && (
-						<button
-							onClick={() => {
-								setShowDesc(!showDesc);
-							}}
-							type="button"
-							className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
-							{showDesc ? "Hide" : "Show"} Description
-						</button>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  const [showDesc, setShowDesc] = useState(false);
+  const [parent] = useAutoAnimate();
+  return (
+    <div className="flex justify-center">
+      <div className="rounded-lg shadow-lg bg-neutral-800 max-w-sm">
+        <img className="rounded-t-lg" src={imgsrc} alt="" />
+        <div className="p-6">
+          <h5 className="text-white text-xl font-bold mb-2">{name}</h5>
+          <h5 className="text-gray-400 text-base font-bold mb-2">{datetime}</h5>
+          <p className="text-gray-400 text-base mb-4" ref={parent}>
+            {showDesc ? (
+              desc
+            ) : (
+              <>
+                Club - {club}
+                <br />
+                Team Size - {tsize}
+                <br />
+                Venue - {venue}
+                <br />
+                Coordinators - {coordinators}
+              </>
+            )}
+          </p>
+          {desc && (
+            <button
+              onClick={() => {
+                setShowDesc(!showDesc);
+              }}
+              type="button"
+              className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              {showDesc ? "Hide" : "Show"} Description
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,28 @@
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
+import NextNProgress from "nextjs-progressbar";
 
-import '../styles/navbar.css';
-import '../styles/globals.css';
-import 'animate.css'
+import "../styles/navbar.css";
+import "../styles/globals.css";
+import "animate.css";
 
-import Head from 'next/head';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
+      <NextNProgress
+        color="#EA580C"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Head>
         <style>
           @import
           url(https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic);
-          @import url(https://fonts.googleapis.com/css?family=Staatliches:regular);
+          @import
+          url(https://fonts.googleapis.com/css?family=Staatliches:regular);
         </style>
       </Head>
       <Navbar />
